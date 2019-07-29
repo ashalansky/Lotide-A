a9f07c42d5345669fd6079cd01e905642b5d9ca3
Amanda Shalansky <ashalansky20@hotmail.com>
2019-07-29 22:36:32 +0000

    refactored with chai

--- test1/middleTest.js
+++ test1/middleTest.js
@@ -1,4 +1,9 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-console.log(middle([1,2,3,4,5]));
+describe("#middle", () => {
+  it("returns 3 for [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3])
+  });
+});
+

878df5fb2b3be63a0edb42611c8ad6884c89077d
Amanda Shalansky <ashalansky20@hotmail.com>
2019-07-29 22:37:27 +0000

    refactored with chai

--- tail1.js
+++ tail1.js
@@ -1,13 +1,16 @@
 const assertEqual = require('./assertEqual');
-const array = [1,2,3];
 const tail = function(array) {
-  if (array.length > 1) {
+  let newArray = array.slice(1);
+  return newArray
+};
+ /*  if (array.length > 1) {
     console.log(array.slice(1));
   } else {
     return [];
   }
-}
-tail(array);
-// assertEqual(tail([1]));
+} */
+
+//tail(array);
+//assertEqual(tail([1]));
 
 module.exports = tail;
